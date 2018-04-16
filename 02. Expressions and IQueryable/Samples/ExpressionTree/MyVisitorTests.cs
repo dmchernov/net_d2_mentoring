72ca6c6ea7f8729f1a3020f57da89f10db8feabb
Dmitrii Chernov1 <Dmitrii_Chernov1@epam.com>
2018-04-16 08:43:05 +0400

    Visitor has benn completed

--- 02. Expressions and IQueryable/Samples/ExpressionTree/MyVisitorTests.cs
+++ 02. Expressions and IQueryable/Samples/ExpressionTree/MyVisitorTests.cs
@@ -5,63 +5,75 @@ using NUnit.Framework;
 
 namespace ExpressionTree
 {
-    [TestFixture]
-    public class MyVisitorTests
-    {
-        [Test]
-        public void IncrementTest()
-        {
-            Expression<Func<int, int>> source = a => a + (a + 1) * (a + 5) * (a + 1);
-            var result = new MyVisitor().VisitAndConvert(source, "");
+	[TestFixture]
+	public class MyVisitorTests
+	{
+		[Test]
+		public void IncrementTest()
+		{
+			Expression<Func<int, int>> source = a => a + (a + 1) * (a + 5) * (a + 1);
+			var result = new MyVisitor().VisitAndConvert(source, "");
 
-            Console.WriteLine(source + " " + source.Compile().Invoke(3));
-            Console.WriteLine(result + " " + result?.Compile().Invoke(3));
-        }
+			Console.WriteLine(source + " " + source.Compile().Invoke(3));
+			Console.WriteLine(result + " " + result?.Compile().Invoke(3));
+		}
 
-        [Test]
-        public void DecrementTest()
-        {
-            Expression<Func<int, int>> source = a => a + (a - 1) * (a + 5) * (a - 1);
-            var result = new MyVisitor().VisitAndConvert(source, "");
+		[Test]
+		public void DecrementTest()
+		{
+			Expression<Func<int, int>> source = a => a + (a - 1) * (a + 5) * (a - 1);
+			var result = new MyVisitor().VisitAndConvert(source, "");
 
-            Console.WriteLine(source + " " + source.Compile().Invoke(3));
-            Console.WriteLine(result + " " + result?.Compile().Invoke(3));
-        }
+			Console.WriteLine(source + " " + source.Compile().Invoke(3));
+			Console.WriteLine(result + " " + result?.Compile().Invoke(3));
+		}
 
-        [Test]
-        public void ParameterCollectionTest_Int()
-        {
-            Expression<Func<int, int>> expression = a => a * 2 + a - a * 3;
-            var values = new Dictionary<string, object>{["a"] = 1};
+		[Test]
+		public void ParameterCollectionIntTest_Int()
+		{
+			Expression<Func<int, int>> expression = a => a * 10 + a - a * 3;
+			var values = new Dictionary<string, object>{["a"] = 1};
 
-            var result = new MyVisitor(values).VisitAndConvert(expression, "");
+			var result = new MyVisitor(values).Visit(expression) as LambdaExpression;
 
-            Console.WriteLine(expression + " " + expression.Compile().Invoke(3));
-            Console.WriteLine(result + " " + result?.Compile().Invoke(3));
-        }
+			Console.WriteLine(expression + " " + expression.Compile().Invoke(1));
+			Console.WriteLine(result + " " + result?.Compile().DynamicInvoke());
+		}
 
-        [Test]
-        public void ParameterCollectionTest_IntWithoutParameters()
-        {
-            Expression<Func<int>> expression = () => 2 * 2 + 2 - 2 * 3;
-            var values = new Dictionary<string, object>{["a"] = 1};
+		[Test]
+		public void ParameterCollectionStringTest_Int()
+		{
+			Expression<Func<string, string, string>> expression = (a, b) => String.Concat(a, b);
+			var values = new Dictionary<string, object>{["a"] = "str1", ["b"] = "str2"};
 
-            var result = new MyVisitor(values).VisitAndConvert(expression, "");
+			var result = new MyVisitor(values).Visit(expression) as LambdaExpression;
 
-            Console.WriteLine(expression + " " + expression.Compile().Invoke());
-            Console.WriteLine(result + " " + result?.Compile().Invoke());
-        }
+			Console.WriteLine(expression + " " + expression.Compile().Invoke("str1", "str2"));
+			Console.WriteLine(result + " " + result?.Compile().DynamicInvoke());
+		}
 
-        [Test]
-        public void ParameterCollectionTest_String()
-        {
-            Expression<Func<string, string>> expression = a => a.ToUpper();
-            var values = new Dictionary<string, object>{["a"] = "aaa"};
+		[Test]
+		public void ParameterCollectionStringIntTest_Int()
+		{
+			Expression<Func<int, string, string>> expression = (a, b) => a.ToString().ToUpper() + b.ToLower();
+			var values = new Dictionary<string, object>{["a"] = 12345, ["b"] = "STRING"};
 
-            var result = new MyVisitor(values).VisitAndConvert(expression, "");
+			var result = new MyVisitor(values).Visit(expression) as LambdaExpression;
 
-            Console.WriteLine(expression + " " + expression.Compile().Invoke("aaa"));
-            Console.WriteLine(result + " " + result?.Compile().Invoke("aaa"));
-        }
-    }
+			Console.WriteLine(expression + " " + expression.Compile().Invoke(12345, "STRING"));
+			Console.WriteLine(result + " " + result?.Compile().DynamicInvoke());
+		}
+
+		[Test]
+		public void ParameterCollectionTest_String()
+		{
+			Expression<Func<string, string>> expression = a => a.ToUpper();
+			var values = new Dictionary<string, object>{["a"] = "aaa"};
+
+			var result = new MyVisitor(values).Visit(expression) as LambdaExpression;
+
+			Console.WriteLine(expression + " " + expression.Compile().Invoke("aaa"));
+			Console.WriteLine(result + " " + result?.Compile().DynamicInvoke());
+		}
+	}
 }
